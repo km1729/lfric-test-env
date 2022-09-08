e1ca7f7376169ac06893f058c66291302b022d6b
kelly <kellymcintyre.dev@gmail.com>
2022-09-08 16:38:34 +1000

    sql connection test

---
+++ lfric-test/code/.ipynb_checkpoints/server_settings-checkpoint.py
@@ -0,0 +1,15 @@
+postgresql = {'pguser':'postgres',
+              'pgpasswd':1234,
+              'pghost':'postgres',
+              'pgport': 5432,
+              'pgdb': 'postgres'
+             }
+
+
+expression = {
+
+}
+
+filePath = {
+    
+}
